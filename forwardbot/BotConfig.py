a0b9e7053c8f2aec14bac24d4dc75c128c3be568
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-07-09 22:19:03 +0530

    Add files via upload

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -1,10 +1,10 @@
 from os import environ
 class Config(object):
-    API_ID = environ.get("API_ID", "20204401")
-    API_HASH = environ.get("API_HASH", "6ec374e7e7492de06a9b22bb746bd20f")
+    API_ID = environ.get("API_ID", "22179597")
+    API_HASH = environ.get("API_HASH", "72a54a3b2a449b5b7dd0c75da9a1487d")
     BOT_TOKEN = environ.get("BOT_TOKEN", "6922036916:AAHod6WEiUf70eq6ytG2P1uRrk6__uQ-xHU")
-    STRING_SESSION = environ.get("STRING_SESSION", "")
-    SUDO_USERS = environ.get("SUDO_USERS", "6168162777 6366990600")
+    STRING_SESSION = environ.get("STRING_SESSION", "1BVtsOGcBuzvo7Cud7jc2GuXckkGLzdzEFJTMbYku6SMAB4110h9NPtiuMjhIWvsm_CurmgFE5ws4Y2aQQrub5x6kq2HEisTp96r59-X9bXKE27RfnggNFenUPNBEdk6ffwx5KcXoJH_SHILVJ72_fRx5f4xOIgG-ZeO-GhqkWubEspYSXXc5pyJq91ule8BA6-3JsqYfZXF7CX-Uw6Df1zBZ8F97QHZkLdJsehY2SOFlN2ATuioy7-60DCKuvLRV1KQALAYNY5CtaInvtYSllKy4L97EYqgdpBl-DXdXDA251MCxv3FPKCtM-70l4bFiS6oRDugPaTyk3EWChUQqt9MnlKjyQac=")
+    SUDO_USERS = environ.get("SUDO_USERS", "6473618718,7037451646")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
